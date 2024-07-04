7d53dab96419c0d76202a489dfc173fff06e80a5
nirfana <devi.nirfana12@gmail.com>
2024-07-04 17:50:03 +0700

    add update csv feature

--- deployment/predict.py
+++ deployment/predict.py
@@ -71,31 +71,66 @@ def predict_sentiment(review_text, model):
     # Convert tensor to a list of 1s and 0s
     predictions_list = predictions.tolist()
     
-    # Replace 1 with 'Recommending' and 0 with 'Not Recommending'
-    # predictions_recommended = ['The author recommending this product' if x == 1 else 'The author not recommending this product' for x in predictions_list]
-    
     return predictions_list
+
+def response(x):
+    if x in ([0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 1, 1]):
+        return 'Requires Immediate Attention'
+    else:
+        return 'Follow The Standard Time Protocol'
+
+def process_csv(file, model):
+    # Read CSV
+    data = pd.read_csv(file)
+    data['complaint_id'] = data['complaint_id'].astype(str)
+    
+    # Preprocess text and predict sentiment
+    data['narrative'] = data['issue'] + ' ' + data['sub_issue'] + ' ' + data['consumer_complaint_narrative']
+    
+    sentences = data['narrative'].apply(text_preprocessing)
+    pred = predict_sentiment(sentences, model)
+    
+    # Apply the response function to each element in pred
+    responses = [response(x) for x in pred]
+    data['response'] = responses
+
+    # Select required columns and format predictions
+    output = data[['complaint_id', 'product', 'response']]
+    return output
 
 def app():
     st.title('Predict Complaint Level')
-    
-    data = pd.read_csv('data/consumer_complaints_cleaned.csv')
 
-    issue = st.selectbox(
-        label='Choose the issue',
-        options=data['issue'].unique()
-    )
-    
-    sub_issue = st.selectbox(
-        label='Choose the sub issue',
-        options=data[data['issue']==issue]['sub_issue'].unique()
+    option = st.selectbox(
+        'Choose input method',
+        ('Upload CSV', 'Manual Input')
     )
 
-    complaint = st.text_area(
-        label="Write Your Complaint"
-    )
+    if option == 'Upload CSV':
+        uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-    narative = issue + ' ' + sub_issue + ' ' + complaint
+        if uploaded_file is not None:
+            with st.spinner('Processing...'):
+                output = process_csv(uploaded_file, model)
+                st.dataframe(output)
+    elif option == 'Manual Input':
+        data = pd.read_csv('data/consumer_complaints_cleaned.csv')
+
+        issue = st.selectbox(
+            label='Choose the issue',
+            options=data['issue'].unique()
+        )
+        
+        sub_issue = st.selectbox(
+            label='Choose the sub issue',
+            options=data[data['issue']==issue]['sub_issue'].unique()
+        )
+
+        complaint = st.text_area(
+            label="Write Your Complaint"
+        )
+
+        narative = issue + ' ' + sub_issue + ' ' + complaint
     
     if st.button("Predict"):
         if complaint:
@@ -105,9 +140,11 @@ def app():
                 sentence = text_preprocessing(narative)
                 
                 predictions = predict_sentiment(sentence, model)
+                
+                responses = response(predictions)
     
                 # Display the prediction result
-                st.success(f'Prediction: {predictions[0]}')
+                st.success(f'Prediction: {responses}')
         else:
             st.warning("Please enter a review.")
 
